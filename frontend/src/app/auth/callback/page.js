3f7394dae8f4b2c9ac496f071d53de996ff374a1
Farooq Ahmed <farooq0319ahmed@gmail.com>
2026-02-20 21:47:22 +0500

    fix: correct use client directive

--- frontend/src/app/auth/callback/page.js
+++ frontend/src/app/auth/callback/page.js
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-
 'use client';
+
+export const dynamic = "force-dynamic";
 
 import React, { Suspense, useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
